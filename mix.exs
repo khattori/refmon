27fe51209f282404334ca098d8d1ea9a2f1281d5
Kenta HATTORI <kenta_hattori@hotmail.com>
2022-02-04 11:28:32 +0900

    Bump up Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Refmon.MixProject do
     [
       app: :refmon,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
@@ -27,7 +27,7 @@ defmodule Refmon.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:cachex, "~> 3.4"},
-      {:excoveralls, "~> 0.13.3", only: :test}
+      {:excoveralls, "~> 0.14", only: :test}
     ]
   end
 
